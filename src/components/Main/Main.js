2bf76995b17fbb9383b9fe16b7ab6b183ceab9fa
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-12-01 00:45:25 -0800

    minor style fixes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Main.css'
 import { signs } from '../../../src/data'
+import ZodiacCard from '../ZodiacCard/ZodiacCard'
+
+import bg from '../../../src/bg.jpg'
 
 export default function Main() {
-  return <main>[hello from Main]{console.log(signs)}</main>
+  return <main style={{ backgroundImage: `url(${bg})` }}>hello from main</main>
 }
