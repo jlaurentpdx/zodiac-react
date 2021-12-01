0035b09055f9f14f93a77031a90e34c958fbf8f1
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-12-01 00:48:48 -0800

    ZodiacCard components rendering to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,5 +6,11 @@ import ZodiacCard from '../ZodiacCard/ZodiacCard'
 import bg from '../../../src/bg.jpg'
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${bg})` }}>hello from main</main>
+  return (
+    <main style={{ backgroundImage: `url(${bg})` }}>
+      {signs.map((sign) => (
+        <ZodiacCard key={sign.name} {...sign} />
+      ))}
+    </main>
+  )
 }
