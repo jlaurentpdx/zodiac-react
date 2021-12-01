c7b687f3c2ee05767e41521110116a3782911d2f
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-11-30 17:52:11 -0800

    initialized data table including zodiac signs

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const signs = [
+  {
+    name: 'aquarius',
+    dates: 'jan 21 - feb 19',
+  },
+  {
+    name: 'aries',
+    dates: 'mar 21 - apr 19',
+  },
+  {
+    name: 'cancer',
+    dates: 'jun 21 - jul 22',
+  },
+  {
+    name: 'capricorn',
+    dates: 'dec 22 - jan 20',
+  },
+  {
+    name: 'gemini',
+    dates: 'may 21 - jun 20',
+  },
+  {
+    name: 'leo',
+    dates: 'jul 23 - aug 22',
+  },
+  {
+    name: 'libra',
+    dates: 'sep 23 - oct 22',
+  },
+  {
+    name: 'pisces',
+    dates: 'feb 20 - mar 20',
+  },
+  {
+    name: 'sagittarius',
+    dates: 'nov 22 - dec 21',
+  },
+  {
+    name: 'scorpio',
+    dates: 'oct 23 - nov 21',
+  },
+  {
+    name: 'taurus',
+    dates: 'apr 20 - may 20',
+  },
+  {
+    name: 'virgo',
+    dates: 'aug 23 - sep 22',
+  },
+]
